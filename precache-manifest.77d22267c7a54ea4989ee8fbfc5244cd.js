7795468ff08b103cd4fd065aabc7ec29627ec77b
Wilson <wc683@nyu.edu>
2019-05-21 16:49:43 -0400

    Updates

---
+++ precache-manifest.77d22267c7a54ea4989ee8fbfc5244cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457a78e50b442326b67b15545562a2ad",
+    "url": "/my-react-starter/index.html"
+  },
+  {
+    "revision": "b2883f9e63c08ff776ed",
+    "url": "/my-react-starter/static/css/main.6afc6fc6.chunk.css"
+  },
+  {
+    "revision": "8a63643581627c825af5",
+    "url": "/my-react-starter/static/js/2.327003c3.chunk.js"
+  },
+  {
+    "revision": "b2883f9e63c08ff776ed",
+    "url": "/my-react-starter/static/js/main.343f3bd7.chunk.js"
+  },
+  {
+    "revision": "f3e36a372ba713a86eeb",
+    "url": "/my-react-starter/static/js/runtime~main.ed10a814.js"
+  }
+]);
